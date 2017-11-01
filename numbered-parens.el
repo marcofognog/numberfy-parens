b61a4695e27f5514ec15ed1a72d70594cacde8d8
Marcao <marcofognog@gmail.com>
2017-11-01 09:42:55 -0200

    One more case: two levels of nesting

--- numbered-parens.el
+++ numbered-parens.el
@@ -2,12 +2,19 @@
   (progn
     (setq new-list (list))
     (setq char-list (split-string original ""))
+    (setq current-level -1)
     (loop for char in char-list collect
           (progn
             (if (string-equal char "(")
-                (setq to-be-added "0")
+                (progn
+                  (setq current-level (+ current-level 1))
+                  (setq to-be-added (number-to-string current-level))
+                  )
               (if (string-equal char ")")
-                  (setq to-be-added "0")
+                  (progn
+                    (setq to-be-added (number-to-string current-level))
+                    (setq current-level (- current-level 1))
+                    )
                 (setq to-be-added char)
                 )
               )
@@ -16,8 +23,14 @@
     (mapconcat 'identity (reverse new-list) "")))
 
 
-(ert-deftest numbered-parens-convert-test ()
+(ert-deftest numbered-parens-convert-0-test ()
   (setq original "(+ 1 2)")
   (setq expected "0+ 1 20")
+  (should (string-equal expected (numbered-parens-convert original )))
+  )
+
+(ert-deftest numbered-parens-convert-1-test ()
+  (setq original "(= (+ 1 2) 5)")
+  (setq expected "0= 1+ 1 21 50")
   (should (string-equal expected (numbered-parens-convert original )))
   )

0d7b3898e9ebbae3a5855f282c055c3edb3c6ec9
Marcao <marcofognog@gmail.com>
2017-11-02 08:33:28 -0200

    better sample

--- sample.el
+++ sample.el
@@ -1,12 +1,12 @@
 (defun numbered-parens-numberfy()
   (interactive)
   (progn
-    (setq content (buffer-substring-no-properties (point-min) (point-max(((
-    (setq new-content (nth 0 (numbered-parens-convert content)((
-    (setq numbered-parens-positions (nth 1 (numbered-parens-convert content()(
-    (setq saved-pos (point()
-    (erase-buffer(
+    (setq content (buffer-substring-no-properties (point-min) (point-max)))
+    (setq new-content (nth 0 (numbered-parens-convert content)))
+    (setq numbered-parens-positions (nth 1 (numbered-parens-convert content)))
+    (setq saved-pos (point))
+    (erase-buffer)
     (insert new-content)
-    (goto-char saved-pos(
+    (goto-char saved-pos)
     )
   )
